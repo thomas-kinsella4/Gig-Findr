be6f47bc753f4ef5e55b0477de471e34176f98ea
Thomas Kinsella <thomasjkinsella4@gmail.com>
2022-06-01 22:36:44 -0400

    finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,25 +16,25 @@ puts("making new")
 #     video: nil
 # )
 
-Artist.create(
-    username: "We$t coa$t we$",
-    password: "pass",
-    profile_img: nil,
-    cover_img: nil,
-    bio: "not gon' hate it, long hair don't care",
-    socialmedia_links: nil,
-    video: nil
-)
+# Artist.create(
+#     username: "We$t coa$t we$",
+#     password: "pass",
+#     profile_img: nil,
+#     cover_img: nil,
+#     bio: "not gon' hate it, long hair don't care",
+#     socialmedia_links: nil,
+#     video: nil
+# )
 
-Artist.create(
-    username: "Poppa Crespo",
-    password: "pass",
-    profile_img: nil,
-    cover_img: nil,
-    bio: nil,
-    socialmedia_links: nil,
-    video: nil
-)
+# Artist.create(
+#     username: "Poppa Crespo",
+#     password: "pass",
+#     profile_img: nil,
+#     cover_img: nil,
+#     bio: nil,
+#     socialmedia_links: nil,
+#     video: nil
+# )
 
 Agent.create(
     username: "Agent1",
@@ -61,12 +61,12 @@ end
 
 Gig.create(
     agent_id: 1,
-    venue: "The Bitter End",
+    venue: "My basement",
     time: 8,
     timetwo: "pm",
     date: "06/03/2022",
-    genres: "Rock, Indie, Alternative",
-    description: "Rock show baby"
+    genres: "House, Electronic, Techno",
+    description: "Techno show baby"
 )
 
 Gig.create(
@@ -209,7 +209,117 @@ Gig.create(
 )
 
 Gig.create(
+    agent_id: 2,
+    venue: "Pianos NYC",
+    time: 10,
+    timetwo: "pm",
+    date: "06/24/2022",
+    genres: "Folk, Bluegrass, Country",
+    description: "Country show baby"
+)
+
+Gig.create(
+    agent_id: 2,
+    venue: "Pianos NYC",
+    time: 9,
+    timetwo: "pm",
+    date: "06/24/2022",
+    genres: "Folk, Bluegrass, Country",
+    description: "Country show baby"
+)
+
+Gig.create(
+    agent_id: 2,
+    venue: "Pianos NYC",
+    time: 8,
+    timetwo: "pm",
+    date: "06/24/2022",
+    genres: "Folk, Bluegrass, Country",
+    description: "Country show baby"
+)
+
+Gig.create(
     agent_id: rand(1..4),
+    venue: "Mercury Lounge",
+    time: 8,
+    timetwo: "pm",
+    date: "10/11/2022",
+    genres: "Country",
+    description: "Country show baby"
+)
+
+Gig.create(
+    agent_id: rand(1..4),
+    venue: "Mercury Lounge",
+    time: 8,
+    timetwo: "pm",
+    date: "10/11/2022",
+    genres: "Country",
+    description: "Country show baby"
+)
+
+Gig.create(
+    agent_id: rand(1..4),
+    venue: "Mercury Lounge",
+    time: 8,
+    timetwo: "pm",
+    date: "10/11/2022",
+    genres: "Country",
+    description: "Country show baby"
+)
+
+Gig.create(
+    agent_id: rand(1..4),
+    venue: "Mercury Lounge",
+    time: 8,
+    timetwo: "pm",
+    date: "10/11/2022",
+    genres: "Country",
+    description: "Country show baby"
+)
+
+Gig.create(
+    agent_id: rand(1..4),
+    venue: "Mercury Lounge",
+    time: 8,
+    timetwo: "pm",
+    date: "10/11/2022",
+    genres: "Country",
+    description: "Country show baby"
+)
+
+Gig.create(
+    agent_id: 2,
+    venue: "Mercury Lounge",
+    time: 8,
+    timetwo: "pm",
+    date: "10/11/2022",
+    genres: "Country",
+    description: "Country show baby"
+)
+
+Gig.create(
+    agent_id: 2,
+    venue: "Mercury Lounge",
+    time: 8,
+    timetwo: "pm",
+    date: "10/11/2022",
+    genres: "Country",
+    description: "Country show baby"
+)
+
+Gig.create(
+    agent_id: 2,
+    venue: "Mercury Lounge",
+    time: 8,
+    timetwo: "pm",
+    date: "10/11/2022",
+    genres: "Country",
+    description: "Country show baby"
+)
+
+Gig.create(
+    agent_id: 2,
     venue: "Mercury Lounge",
     time: 8,
     timetwo: "pm",
@@ -223,7 +333,7 @@ Gig.create(
     venue: "Mercury Lounge",
     time: 8,
     timetwo: "pm",
-    date: "06/11/2022",
+    date: "10/11/2022",
     genres: "Country",
     description: "Country show baby"
 )
@@ -233,13 +343,13 @@ Gig.create(
     venue: "Mercury Lounge",
     time: 8,
     timetwo: "pm",
-    date: "06/11/2022",
+    date: "10/11/2022",
     genres: "Country",
     description: "Country show baby"
 )
 
 Gig.create(
-    agent_id: rand(1..4),
+    agent_id: 2,
     venue: "Mercury Lounge",
     time: 8,
     timetwo: "pm",
@@ -249,7 +359,7 @@ Gig.create(
 )
 
 Gig.create(
-    agent_id: rand(1..4),
+    agent_id: 2,
     venue: "Mercury Lounge",
     time: 8,
     timetwo: "pm",
@@ -259,13 +369,292 @@ Gig.create(
 )
 
 Gig.create(
+    agent_id: 1,
+    venue: "The Bowery Electric",
+    time: 9,
+    timetwo: "pm",
+    date: "06/12/2022",
+    genres: "Hip Hop, Trap",
+    description: "Rap show baby"
+)
+
+Gig.create(
+    agent_id: 2,
+    venue: "Pianos NYC",
+    time: 11,
+    timetwo: "pm",
+    date: "06/24/2022",
+    genres: "Folk, Bluegrass, Country",
+    description: "Country show baby"
+)
+
+Gig.create(
+    agent_id: 3,
+    venue: "The Delancey",
+    time: 8,
+    date: "06/28/2022",
+    genres: "Electronic, Indie, EDM",
+    description: "Show baby"
+)
+
+Gig.create(
+    agent_id: 3,
+    venue: "The Delancey",
+    time: 7,
+    timetwo: "pm",
+    date: "06/28/2022",
+    genres: "Electronic, Indie, EDM",
+    description: "Show baby"
+)
+
+Gig.create(
+    agent_id: 3,
+    venue: "The Delancey",
+    time: 9,
+    timetwo: "pm",
+    date: "06/28/2022",
+    genres: "Electronic, Indie, EDM",
+    description: "Show baby"
+)
+
+Gig.create(
+    agent_id: 3,
+    venue: "The Delancey",
+    time: 10,
+    timetwo: "pm",
+    date: "06/28/2022",
+    genres: "Electronic, Indie, EDM",
+    description: "Show baby"
+)
+
+Gig.create(
+    agent_id: 3,
+    venue: "The Delancey",
+    time: 11,
+    timetwo: "pm",
+    date: "06/28/2022",
+    genres: "Electronic, Indie, EDM",
+    description: "Show baby"
+)
+
+Gig.create(
     agent_id: rand(1..4),
-    venue: "Mercury Lounge",
+    venue: "Rockwood Music Hall",
+    time: 10,
+    timetwo: "pm",
+    date: "07/30/2022",
+    genres: "Rock, Hard Rock",
+    description: "Rock show baby"
+)
+
+Gig.create(
+    agent_id: rand(1..4),
+    venue: "Arlene's Grocery",
     time: 8,
     timetwo: "pm",
-    date: "06/11/2022",
+    date: "08/02/2022",
+    genres: "Heavy Metal, Hard Rock, Metal",
+    description: "Metal show baby"
+)
+
+Gig.create(
+    agent_id: 2,
+    venue: "Baby's Alright BK",
+    time: 10,
+    timetwo: "pm",
+    date: "08/16/2022",
+    genres: "Pop, Indie Pop, Dance",
+    description: "Dance show baby"
+)
+
+Gig.create(
+    agent_id: 2,
+    venue: "Baby's Alright BK",
+    time: 9,
+    timetwo: "pm",
+    date: "08/16/2022",
+    genres: "Pop, Indie Pop, Dance",
+    description: "Dance show baby"
+)
+
+Gig.create(
+    agent_id: 2,
+    venue: "Baby's Alright BK",
+    time: 8,
+    timetwo: "pm",
+    date: "08/16/2022",
+    genres: "Pop, Indie Pop, Dance",
+    description: "Dance show baby"
+)
+
+Gig.create(
+    agent_id: 3,
+    venue: "Elsewhere BK",
+    time: 10,
+    timetwo: "pm",
+    date: "07/17/2022",
+    genres: "House, Electronic, Electronic Indie",
+    description: "Electronic show baby"
+)
+
+Gig.create(
+    agent_id: 3,
+    venue: "Elsewhere BK",
+    time: 11,
+    timetwo: "pm",
+    date: "07/17/2022",
+    genres: "House, Electronic, Electronic Indie",
+    description: "Electronic show baby"
+)
+
+Gig.create(
+    agent_id: rand(1..4),
+    venue: "Knitting Factory BK",
+    time: 10,
+    timetwo: "pm",
+    date: "08/10/2022",
+    genres: "Classical Music, Beethoven",
+    description: "Beethoven show baby"
+)
+
+Gig.create(
+    agent_id: rand(1..4),
+    venue: "Knitting Factory BK",
+    time: 9,
+    timetwo: "pm",
+    date: "08/10/2022",
+    genres: "Classical Music, Beethoven",
+    description: "Beethoven show baby"
+)
+
+Gig.create(
+    agent_id: rand(1..4),
+    venue: "Knitting Factory BK",
+    time: 8,
+    timetwo: "pm",
+    date: "08/10/2022",
+    genres: "Classical Music, Beethoven",
+    description: "Beethoven show baby"
+)
+
+Gig.create(
+    agent_id: 2,
+    venue: "Wes' house",
+    time: 8,
+    timetwo: "pm",
+    date: "06/17/2022",
     genres: "Country",
     description: "Country show baby"
+)
+
+Gig.create(
+    agent_id: 2,
+    venue: "Wes' house",
+    time: 7,
+    timetwo: "pm",
+    date: "06/17/2022",
+    genres: "Country",
+    description: "Country show baby"
+)
+
+Gig.create(
+    agent_id: 2,
+    venue: "Wes' house",
+    time: 6,
+    timetwo: "pm",
+    date: "06/17/2022",
+    genres: "Country",
+    description: "Country show baby"
+)
+
+Gig.create(
+    agent_id: rand(1..4),
+    venue: "The Lounge",
+    time: 10,
+    timetwo: "pm",
+    date: "07/11/2022",
+    genres: "Jazz",
+    description: "Jazz show baby"
+)
+
+Gig.create(
+    agent_id: rand(1..4),
+    venue: "The Lounge",
+    time: 9,
+    timetwo: "pm",
+    date: "07/11/2022",
+    genres: "Jazz",
+    description: "Jazz show baby"
+)
+
+Gig.create(
+    agent_id: rand(1..4),
+    venue: "The Lounge",
+    time: 8,
+    timetwo: "pm",
+    date: "07/11/2022",
+    genres: "Jazz",
+    description: "Jazz show baby"
+)
+
+Gig.create(
+    agent_id: rand(1..4),
+    venue: "The park next door",
+    time: 10,
+    timetwo: "pm",
+    date: "08/14/2022",
+    genres: "folk, singer/songwriter",
+    description: "folk"
+)
+
+Gig.create(
+    agent_id: rand(1..4),
+    venue: "The park next door",
+    time: 11,
+    timetwo: "pm",
+    date: "06/14/2022",
+    genres: "folk, singer/songwriter",
+    description: "folk"
+)
+
+Gig.create(
+    agent_id: 1,
+    venue: "My basement",
+    time: 10,
+    timetwo: "pm",
+    date: "06/15/2022",
+    genres: "heavy metal",
+    description: "metal show baby"
+)
+
+Gig.create(
+    agent_id: rand(1..4),
+    venue: "The local church",
+    time: 8,
+    timetwo: "pm",
+    date: "07/03/2022",
+    genres: "Soul, Gospel, Choir",
+    description: "Soul show baby"
+)
+
+Gig.create(
+    agent_id: rand(1..4),
+    venue: "The local church",
+    time: 9,
+    timetwo: "pm",
+    date: "07/03/2022",
+    genres: "Soul, Gospel, Choir",
+    description: "Soul show baby"
+)
+
+Gig.create(
+    agent_id: 1,
+    venue: "My basement",
+    time: 10,
+    timetwo: "pm",
+    date: "06/09/2022",
+    genres: "heavy metal, metal, hardcore",
+    description: "metal show baby"
 )
 
 Gig.create(
@@ -273,9 +662,9 @@ Gig.create(
     venue: "Mercury Lounge",
     time: 8,
     timetwo: "pm",
-    date: "06/11/2022",
-    genres: "Country",
-    description: "Country show baby"
+    date: "08/11/2022",
+    genres: "Blues",
+    description: "Blues show baby"
 )
 
 Gig.create(
@@ -283,9 +672,9 @@ Gig.create(
     venue: "Mercury Lounge",
     time: 8,
     timetwo: "pm",
-    date: "06/11/2022",
-    genres: "Country",
-    description: "Country show baby"
+    date: "08/11/2022",
+    genres: "Blues",
+    description: "Blues show baby"
 )
 
 Gig.create(
@@ -293,9 +682,9 @@ Gig.create(
     venue: "Mercury Lounge",
     time: 8,
     timetwo: "pm",
-    date: "06/11/2022",
-    genres: "Country",
-    description: "Country show baby"
+    date: "08/11/2022",
+    genres: "Blues",
+    description: "Blues show baby"
 )
 
 Gig.create(
@@ -303,9 +692,9 @@ Gig.create(
     venue: "Mercury Lounge",
     time: 8,
     timetwo: "pm",
-    date: "06/11/2022",
-    genres: "Country",
-    description: "Country show baby"
+    date: "08/11/2022",
+    genres: "Blues",
+    description: "Blues show baby"
 )
 
 Gig.create(
@@ -313,9 +702,9 @@ Gig.create(
     venue: "Mercury Lounge",
     time: 8,
     timetwo: "pm",
-    date: "06/11/2022",
-    genres: "Country",
-    description: "Country show baby"
+    date: "08/11/2022",
+    genres: "Blues",
+    description: "Blues show baby"
 )
 
 Gig.create(
@@ -323,51 +712,10 @@ Gig.create(
     venue: "Mercury Lounge",
     time: 8,
     timetwo: "pm",
-    date: "06/11/2022",
-    genres: "Country",
-    description: "Country show baby"
+    date: "08/11/2022",
+    genres: "Blues",
+    description: "Blues show baby"
 )
-
-Gig.create(
-    agent_id: rand(1..4),
-    venue: "Mercury Lounge",
-    time: 8,
-    timetwo: "pm",
-    date: "06/11/2022",
-    genres: "Country",
-    description: "Country show baby"
-)
-
-Gig.create(
-    agent_id: rand(1..4),
-    venue: "Mercury Lounge",
-    time: 8,
-    timetwo: "pm",
-    date: "06/11/2022",
-    genres: "Country",
-    description: "Country show baby"
-)
-
-Gig.create(
-    agent_id: rand(1..4),
-    venue: "Mercury Lounge",
-    time: 8,
-    timetwo: "pm",
-    date: "06/11/2022",
-    genres: "Country",
-    description: "Country show baby"
-)
-
-Gig.create(
-    agent_id: rand(1..4),
-    venue: "Mercury Lounge",
-    time: 8,
-    timetwo: "pm",
-    date: "06/11/2022",
-    genres: "Country",
-    description: "Country show baby"
-)
-
 
 
 puts("done seeding")
